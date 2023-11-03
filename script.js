e8ff7b094f88855be251cf2a542d8ffd2f823539
lijachang <127257247+lijachang@users.noreply.github.com>
2023-11-03 16:05:12 -0400

    Update script.js
    update api key

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ const chatInput = document.querySelector(".chat-input textarea");
 const sendChatBtn = document.querySelector(".chat-input span");
 
 let userMessage = null; // Variable to store user's message
-const API_KEY = "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"; // Paste your API key here
+const API_KEY = "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"; // Paste your API key here
 const inputInitHeight = chatInput.scrollHeight;
 
 const createChatLi = (message, className) => {
